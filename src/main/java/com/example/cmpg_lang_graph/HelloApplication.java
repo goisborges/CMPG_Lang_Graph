afaeda5d8a655caff6e81e78ad3af0b58c5b12aa
Marcos de Gois Borges <goisborges@gmail.com>
2021-10-12 23:29:01 -0400

    getCompensation View and controller created. View created with SceneBuilder. It'' only missing the SQL statement as I am still working on the database and table on AWS. Properly added Jfoenix items and initial view. Jfoenix gave me some trouble and also my IntelliJ was crashing on MacOs but now it's solved

--- src/main/java/com/example/cmpg_lang_graph/HelloApplication.java
+++ src/main/java/com/example/cmpg_lang_graph/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("devCompensationView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Computer Programming Languages Research Graphs");
         stage.setScene(scene);
         stage.show();
